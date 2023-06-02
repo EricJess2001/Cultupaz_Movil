8a15095d18b5acc275afe96075566d19f599c02c
EricJess2001 <ericjessid2018@gmail.com>
2023-06-02 10:45:54 -0500

    boton de regreso imagenes

--- app/src/main/java/com/example/cultupazmovil/ui/imagens.java
+++ app/src/main/java/com/example/cultupazmovil/ui/imagens.java
@@ -1,24 +1,49 @@
 package com.example.cultupazmovil.ui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.cultupazmovil.R;
+import com.example.cultupazmovil.databinding.FragmentImagensBinding;
+import com.example.cultupazmovil.databinding.FragmentInformaBinding;
 
 
 public class imagens extends Fragment {
 
-
+    private FragmentImagensBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentImagensBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_imagens, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imagens, container, false);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        ImageView regresomenu =(ImageView) view.findViewById(R.id.regresomenu);
+
+         regresomenu.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 menu menusm = new menu();
+
+                 FragmentManager fragmentManager = getFragmentManager();
+
+                 fragmentManager.beginTransaction()
+                         .replace(R.id.nav_host_fragment_activity_main, menusm)
+                         .commit();
+
+             }
+         });
+
+        return view;
     }
 }

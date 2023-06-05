85d436aae929167a1358571b99051f1c3ed522bb
NicoleGalindez <nbgalindez@misena.edu.co>
2023-06-05 17:50:39 -0500

    bases de datos

--- app/src/main/java/com/example/cultupazmovil/ui/Expresate.java
+++ app/src/main/java/com/example/cultupazmovil/ui/Expresate.java
@@ -2,65 +2,90 @@ package com.example.cultupazmovil.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.cultupazmovil.R;
+import com.example.cultupazmovil.databinding.FragmentInformaBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Expresate#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Expresate extends Fragment {
+import java.util.HashMap;
+import java.util.Map;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class Expresate extends DialogFragment {
 
-    public Expresate() {
-        // Required empty public constructor
-    }
+    Button buttonenviar;
+    EditText temaha, expression;
+    private FirebaseFirestore mfirestore;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Expresate.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Expresate newInstance(String param1, String param2) {
-        Expresate fragment = new Expresate();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentInformaBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onCreate (Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentInformaBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_expresate, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expresate, container, false);
+        mfirestore = FirebaseFirestore.getInstance();
+
+        temaha = view.findViewById(R.id.temaha);
+        expression = view.findViewById(R.id.expression);
+
+        buttonenviar = view.findViewById(R.id.buttonenviar);
+
+        buttonenviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = temaha.getText().toString().trim();
+                String description = expression.getText().toString().trim();
+
+
+                if (title.isEmpty() && description.isEmpty()){
+                    Toast.makeText(getContext(), "Ingresar los datos", Toast.LENGTH_SHORT).show();
+                }else{
+                    postData(title,description);
+                }
+
+
+            }
+        });
+        return view;
     }
+
+    private void postData(String title, String description) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("titulo", title);
+        map.put("Expresion", description);
+
+        mfirestore.collection("pet").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(@NonNull DocumentReference documentReference) {
+
+                Toast.makeText(getContext(),"Creado exitosamente", Toast.LENGTH_SHORT).show();
+              getDialog().dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Error al ingresar", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
 }

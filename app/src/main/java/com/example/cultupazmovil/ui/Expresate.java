b351d35bd445bc421de0f592dc552464b119f158
EricJess2001 <ericjessid2018@gmail.com>
2023-06-06 07:17:23 -0500

    arreglo

--- app/src/main/java/com/example/cultupazmovil/ui/Expresate.java
+++ app/src/main/java/com/example/cultupazmovil/ui/Expresate.java
@@ -2,65 +2,37 @@ package com.example.cultupazmovil.ui;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.cultupazmovil.R;
+import com.example.cultupazmovil.databinding.FragmentExpresateBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Expresate#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Expresate extends Fragment {
+public class Expresate extends DialogFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentExpresateBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public Expresate() {
-        // Required empty public constructor
-    }
+    Button button;
+    EditText temaha, expression;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Expresate.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Expresate newInstance(String param1, String param2) {
-        Expresate fragment = new Expresate();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);}
+
+
+
+        @Override
+        public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState){
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_expresate, container, false);
         }
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expresate, container, false);
-    }
-}

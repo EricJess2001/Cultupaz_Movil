5244bf7d4fa3061c2ef67ac3d6214cb2c654ab50
NicoleGalindez <nbgalindez@misena.edu.co>
2023-06-14 18:20:55 -0500

    probando registro firebase

---
+++ app/src/main/java/com/example/cultupazmovil/ui/registradosfire.java
@@ -0,0 +1,118 @@
+package com.example.cultupazmovil.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.cultupazmovil.MainActivity;
+import com.example.cultupazmovil.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class registradosfire extends AppCompatActivity {
+
+    Button btn_register;
+    EditText name, email, password;
+
+    FirebaseFirestore mFirestore;
+
+    FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registradosfire);
+
+        mFirestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+
+        name = findViewById(R.id.nombre);
+        email = findViewById(R.id.correo);
+        password = findViewById(R.id.contraseña);
+        btn_register = findViewById(R.id.btn_registro);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nameUser = name.getText().toString().trim();
+                String emailUser = email.getText().toString().trim();
+                String passUser = password.getText().toString().trim();
+
+                if(nameUser.isEmpty() && emailUser.isEmpty() && passUser.isEmpty()){
+
+                    Toast.makeText(registradosfire.this, "Complete los datos", Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    registerUser(nameUser, emailUser, passUser);
+
+
+                }
+
+
+            }
+
+            private void registerUser(String nameUser, String emailUser, String passUser) {
+
+                mAuth.createUserWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        String id = mAuth.getCurrentUser().getUid();
+
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("id", id);
+                        map.put("name", nameUser);
+                        map.put("email", emailUser);
+                        map.put("password", passUser);
+
+
+                        mFirestore.collection("user").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+
+                                finish();
+
+                                startActivity(new Intent(registradosfire.this, MainActivity.class));
+                                Toast.makeText(registradosfire.this, "Usuario registrado con exito", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                                Toast.makeText(registradosfire.this, "Error al guardar", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(registradosfire.this, "Error al registrar", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+    }
+}

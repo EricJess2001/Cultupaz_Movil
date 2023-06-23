bc253d4d12ad16dc37f527a4e08c39ad87fe7658
karenmorales21 <mariakarenmorales2108@gmail.com>
2023-06-23 14:21:29 -0500

    subo inicio_Admin

---
+++ app/src/main/java/com/example/cultupazmovil/ui/inicio_Admin.java
@@ -0,0 +1,16 @@
+package com.example.cultupazmovil.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cultupazmovil.R;
+
+public class inicio_Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_admin);
+    }
+}

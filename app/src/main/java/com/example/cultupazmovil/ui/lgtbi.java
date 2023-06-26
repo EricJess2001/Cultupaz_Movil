d91a2b4fb68ed5cf208c596df6b7f426340f940a
karenmorales21 <mariakarenmorales2108@gmail.com>
2023-06-26 10:24:45 -0500

    links

--- app/src/main/java/com/example/cultupazmovil/ui/lgtbi.java
+++ app/src/main/java/com/example/cultupazmovil/ui/lgtbi.java
@@ -1,15 +1,18 @@
 package com.example.cultupazmovil.ui;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.cultupazmovil.R;
 import com.example.cultupazmovil.databinding.FragmentCulturaBinding;
@@ -19,7 +22,7 @@ public class lgtbi extends Fragment {
 
 
     private FragmentLgtbiBinding binding;
-
+TextView textView4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,6 +46,11 @@ public class lgtbi extends Fragment {
                         .commit();
             }
         });
+
+        textView4 = view.findViewById(R.id.textView4);
+        textView4.setMovementMethod(LinkMovementMethod.getInstance());
+        textView4.setLinkTextColor(Color.BLUE);
+
 
         // Inflate the layout for this fragment
         return view;
